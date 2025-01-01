07ae08212ed3cfe263860e30c6a18249096d0825
Alejandro Perez <alejo.perezja@gmail.com>
2024-12-31 19:05:02 -0500

    Arreglo del fondo

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
         <>
             <Header/>
 
-            <div className='fixed w-full flex bg-[url(assets/images/bgDesktop.webp)] h-full bg-no-repeat bg-cover bg-center -z-10'>
+            <div className='fixed w-full flex bg-[url(/assets/images/bgDesktop.webp)] h-full bg-no-repeat bg-cover bg-center -z-10'>
                 <div className='w-full bg-black bg-opacity-60'>
                 </div>
             </div>
@@ -16,7 +16,7 @@ const Layout = ({ children }) => {
 
             <Footer/>
         </>
-    )
-}
+    );
+};
 
-export default Layout
+export default Layout;
